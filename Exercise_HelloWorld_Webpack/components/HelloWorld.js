a01074215772fc2fe04ec825da1f5e08fdf33ec4
vaaher <vaaher@cisco.com>
2022-07-29 17:31:20 -0700

    #Excercise-Hello-World

---
+++ Exercise_HelloWorld_Webpack/components/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+export default function HelloWorld(){
+    return(
+        <div>This is from Hello World Component!</div>
+    );
+}
+//ReactDOM.render(<HelloWorld/>, root);
